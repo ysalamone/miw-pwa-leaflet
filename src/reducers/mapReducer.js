e8b15328d5d63d84629bd456cc8519b040bb106f
Yohan Salamone <yohan.salamone@outlook.com>
2021-03-12 16:07:23 +0100

    id unique timestamp

--- src/reducers/mapReducer.js
+++ src/reducers/mapReducer.js
@@ -1,11 +1,11 @@
-import {generateUniqueID} from "web-vitals/dist/lib/generateUniqueID";
-
 const mapReducer = (state, action) => {
     const { type, data } = action
     switch (type) {
+        case 'toggle_geoloc':
+            return {...state, geolocation: data.geoloc};
         case 'add_marker':
             if(navigator.vibrate) navigator.vibrate(200);
-            return {...state, markers: [...state.markers, { id: generateUniqueID(), latlng: data.clicked } ]}
+            return {...state, markers: [...state.markers, { id: Date.now(), latlng: data.clicked } ]}
         case 'delete_marker':
             return {...state, markers: [...state.markers.filter(marker => marker.id !== data.selected)]}
         default:
