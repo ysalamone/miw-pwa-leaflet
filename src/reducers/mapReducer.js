ed562bee38eed9cecf51d4905b2b39efaa54e722
Yohan Salamone <yohan.salamone@outlook.com>
2021-03-12 15:28:37 +0100

    vérification avant vibration

--- src/reducers/mapReducer.js
+++ src/reducers/mapReducer.js
@@ -4,7 +4,7 @@ const mapReducer = (state, action) => {
     const { type, data } = action
     switch (type) {
         case 'add_marker':
-            window.navigator.vibrate(200);
+            if(navigator.vibrate) navigator.vibrate(200);
             return {...state, markers: [...state.markers, { id: generateUniqueID(), latlng: data.clicked } ]}
         case 'delete_marker':
             return {...state, markers: [...state.markers.filter(marker => marker.id !== data.selected)]}
